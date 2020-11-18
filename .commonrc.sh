cf03d1ba80550f5557bd84a0692a0939a8f3f4c6
Bharath Sriraam R R <albharath.1305@gmail.com>
2020-11-18 16:20:34 +0530

    install neovim and vim-plug

--- .commonrc.sh
+++ .commonrc.sh
@@ -2,6 +2,13 @@
 export LANG=en_US.UTF-8
 export EDITOR='vim'
 
+# NeoVim
+if ! command -v "nvim" >/dev/null 2>&1; then
+    sudo apt-get install -y neovim 
+    sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+fi
+
 # Base16 Shell
 BASE16_SHELL=$HOME/.config/base16-shell/
 source $BASE16_SHELL/scripts/base16-oceanicnext.sh
