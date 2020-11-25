1013ecafa54c660342066935f48d949a459edf7c
Bharath Sriraam R R <albharath.1305@gmail.com>
2020-11-25 19:57:37 +0530

    nvim and golang

--- .commonrc.sh
+++ .commonrc.sh
@@ -1,13 +1,14 @@
 # common env vars
 export LANG=en_US.UTF-8
-export EDITOR='vim'
 
 # NeoVim
 if ! command -v "nvim" >/dev/null 2>&1; then
     sudo apt-get install -y neovim 
     sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'   
 fi
+
+export EDITOR='nvim'
 
 # Base16 Shell
 BASE16_SHELL=$HOME/.config/base16-shell/
@@ -17,3 +18,8 @@ source $BASE16_SHELL/scripts/base16-oceanicnext.sh
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# golang
+export GOROOT=/usr/local/go
+export GOPATH=$HOME/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
