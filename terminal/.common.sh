ebb16318a2ccfb65de7f78ad9db93cb877f84a44
Bharath Sriraam R R <albharath.1305@gmail.com>
2021-11-14 03:11:39 +0530

    Remove .terminal_theme.sh

--- terminal/.common.sh
+++ terminal/.common.sh
@@ -2,9 +2,6 @@
 export LANG=en_US.UTF-8
 export EDITOR='nvim'
 
-# theme
-source "$HOME/.terminal_theme.sh"
-
 # NVM 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
